c622a3ebc58bc782d08c9d572919a1bdc4667af0
Igor Vinokurov <harryvin@bk.ru>
2024-10-01 19:05:00 +0300

    FileGameModelAdded

---
+++ RockScissorsPaper/GameModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockScissorsPaper
+{
+    public class GameModel
+    {
+        private readonly string[] choices = { "Камень", "Ножницы", "Бумага" };
+        private readonly Random random = new();
+
+        public string GetComputerChoice()
+        {
+            return choices[random.Next(choices.Length)];
+        }
+        public string DetermineWinner(string playerChoice, string computerChoice)
+        {
+            if (playerChoice == computerChoice)
+            {
+                return "Ничья!";
+            }
+            if ((playerChoice == "Камень" && computerChoice == "Ножницы") ||
+                (playerChoice == "Ножницы" && computerChoice == "Бумага") ||
+                (playerChoice == "Бумага" && computerChoice == "Камень"))
+            {
+                return "Вы победили!";
+            }
+            return "Вы проиграли!";
+        }
+    }
+}
